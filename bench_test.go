f1a502a10022856b2817e7d70a09acb886106499
George Herbert <george.herbert@icloud.com>
2020-11-23 16:23:32 +0000

    Fixed bugs

--- bench_test.go
+++ bench_test.go
@@ -11,27 +11,6 @@ func getSizes() []int {
 	return []int{16, 64, 128, 256, 512}
 }
 
-func BenchmarkParallel(b *testing.B) {
-	for _, size := range getSizes() {
-		b.Run(fmt.Sprint(size), func(b *testing.B) {
-			os.Stdout = nil // Disable all program output apart from benchmark results
-			params := gol.Params{
-				Turns:       10,
-				Threads:     8,
-				ImageWidth:  size,
-				ImageHeight: size,
-			}
-			keyPresses := make(chan rune, 10)
-			events := make(chan gol.Event, 1000)
-			for i := 0; i < b.N; i++ {
-				b.StartTimer()
-				gol.Run(params, events, keyPresses)
-				b.StopTimer()
-			}
-		})
-	}
-}
-
 func BenchmarkSequential(b *testing.B) {
 	for _, size := range getSizes() {
 		b.Run(fmt.Sprint(size), func(b *testing.B) {
@@ -42,9 +21,30 @@ func BenchmarkSequential(b *testing.B) {
 				ImageWidth:  size,
 				ImageHeight: size,
 			}
-			keyPresses := make(chan rune, 10)
-			events := make(chan gol.Event, 1000)
 			for i := 0; i < b.N; i++ {
+				keyPresses := make(chan rune, 10)
+				events := make(chan gol.Event, 1000)
+				b.StartTimer()
+				gol.Run(params, events, keyPresses)
+				b.StopTimer()
+			}
+		})
+	}
+}
+
+func BenchmarkParallel(b *testing.B) {
+	for _, size := range getSizes() {
+		b.Run(fmt.Sprint(size), func(b *testing.B) {
+			os.Stdout = nil // Disable all program output apart from benchmark results
+			params := gol.Params{
+				Turns:       10,
+				Threads:     8,
+				ImageWidth:  size,
+				ImageHeight: size,
+			}
+			for i := 0; i < b.N; i++ {
+				keyPresses := make(chan rune, 10)
+				events := make(chan gol.Event, 1000)
 				b.StartTimer()
 				gol.Run(params, events, keyPresses)
 				b.StopTimer()
